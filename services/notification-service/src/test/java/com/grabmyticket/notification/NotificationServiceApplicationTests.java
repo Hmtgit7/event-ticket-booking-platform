86e290e407ff2170c24a7eb4a42a8f523257c801
Hmtgit7 <hmtloharcoding3579@gmail.com>
2026-07-31 16:51:57 +0530

    build(notification-service): scaffold service foundation

---
+++ services/notification-service/src/test/java/com/grabmyticket/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.grabmyticket.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
