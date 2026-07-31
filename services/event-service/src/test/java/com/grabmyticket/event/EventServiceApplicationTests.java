b565e9a067bcf747d6093dec659710c268890442
Hmtgit7 <hmtloharcoding3579@gmail.com>
2026-07-31 16:51:28 +0530

    build(event-service): scaffold service foundation

---
+++ services/event-service/src/test/java/com/grabmyticket/event/EventServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.grabmyticket.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class EventServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
